86b92ece16e2b355a46a08fe19d2e0058db4fc80
tamaraandrea <tamara18_1985@msn.com>
2021-11-14 17:37:38 -0500

    think I  broke my code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
  // Array of words
-const words = ['jupiter', 'stars', 'astroid', 'black hole', 'satilite', 'orbit', 'universe', 'umbra', 'lunar', 'space station', 'astronomy', 'uper nova', 'nubula', 'dark matter', 'celestial body']
+const words = ['jupiter', 'stars', 'astroid', 'black hole', 'satilite', 'orbit', 'universe', 'umbra', 'lunar', 'space station', 'astronomy', 'uper nova', 'nebula', 'dark matter', 'celestial body']
 
 // guesses Array
 let myGuesses = []
@@ -7,137 +7,212 @@ let myGuesses = []
 //variables
 let wordSpace = "_"
 let guess = ' '
+let context;
 
 //score
-let numOfTries = 10
+let tries = 10
 let counter ;
 
     
     
     //play function
-    function play() {
-        let userInput = prompt(`would you like to play spaceman? (Y/N)`, "Y")
+function play() {
+    let userInput = prompt(`would you like to play spaceman? (Y/N)`, "Y")
         
-        let index = Math.floor(Math.random()* words.length)
-        console.log(words[index])
+    let index = Math.floor(Math.random()* words.length)
+    console.log(words[index])
         
         
-        for(let i = 0; i < words[index].length; i++){
-            console.log(words[0][i])
+    for(let i = 0; i < words[index].length; i++){
+        console.log(words[0][i])
             
       
 
             
     
             
-            let div = document.createElement('div')
-            div.innerHTML=' - '//words[0][i]
+        let div = document.createElement('div')
+        div.innerHTML=' - '//words[0][i]
             
-            document.querySelector('.word-space').append(div)
+        document.querySelector('.word-space').append(div)
             
     
-            if(userInput === "Y"){
-                if(words[i] === ' - '){
-                    guess.innerHTML = ' - ';
-                    words.appendChild('.spaces')
-                    wordSpace = 5
+         if(userInput === "Y"){
+            if(words[i] === ' - '){
+                guess.innerHTML = ' - '
+                words.appendChild('.spaces')
+                wordSpace = 1
                 
-                } else {
-                    guess.innerHTML = ' - '
+            } 
+            guess.innerHTML = ' - '
     
                 
                 
     
                 
+        }
+    
+    }                                        
+}
+function ans () {
+    const buttons = document.querySelectorAll('button')
+    buttons.forEach(button => button.addEventListener('click', e => {
+        console.log(e.target.innerHTML)
+        myGuesses.push(e.target.innerHTML)
+        console.log(myGuesses)
+        console.log(words[index].includes(e.target.innerHTML))
+        if(words[index].includes(e.target.innerHTML)){
+            document.querySelector('.word-space').innerHTML= '  '
+            for(let i = 0; i < words[index].length; i++){
+                console.log(words[0][i])
+                let div = document.createElement('div')
+                if (myGuesses.includes(words[index][i])){
+                    div.innerHTML=words[index][i]
+                            
+                            
+                            
+                } else if(words[index][i] !== myGuesses.length){
+                    div.innerHTML= ' - '
+                    tries -= 1
+                    getNumOfTries()
+                            
                 }
-    
-        }
-        
-            
-    
-                
-           
-            
-            
-            
-        }
-        function ans () {
-            const buttons = document.querySelectorAll('button')
-            buttons.forEach(button => button.addEventListener('click', e => {
-                console.log(e.target.innerHTML)
-                myGuesses.push(e.target.innerHTML)
-                console.log(myGuesses)
-                console.log(words[index].includes(e.target.innerHTML))
-                if(words[index].includes(e.target.innerHTML)){
-                    document.querySelector('.word-space').innerHTML= '  '
-                    for(let i = 0; i < words[index].length; i++){
-                        console.log(words[0][i])
-                        let div = document.createElement('div')
-                        if (myGuesses.includes(words[index][i])){
-                            div.innerHTML=words[index][i]
-                            
-                            
-                        }else{
-                            div.innerHTML= ' - '
-                        }
-                        document.querySelector('.word-space').append(div)
+                document.querySelector('.word-space').append(div)
                         
-                    }
+            }
                
                 
-                } 
+        }                     
+    }))
+}
+ ans()
 
-                 
-            
+function getNumOfTries (){
+     const showTries = document.querySelector('#myTries')
+    showTries.innerHTML = ' You have ' + tries + ' tries'
+     if(tries < 1){
+         showTries.innerHTML = 'You loose!'
+               
+    }
+    for(i = 0; i < myGuesses.length; i++) {
+        if(words[index][i] + tries[i] === myGuesses.length) {
+            showTries.innerHTML = 'You Win!'
+        }
+    }
+}
+getNumOfTries()
+
+function animateMan () {
+    let drawMan = tries ;
+    drawArray[drawMan]()
+}
+
     
-            }))
-        }
-        ans()
 
-        function getNumOfTries (){
-            const showTries = document.querySelector('#myTries')
-            showTries.innerHTML = ' You have ' + numOfTries + ' numOfTries'
-            if(numOfTries < 1){
-                showTries.innerHTML = 'You loose!'
-               
+function canvas(){
+    let littleMan = document.getElementById('man');
+    let context = littleMan.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = 'white';
+    context.lineWidth = 3;
+}
+
+function head(){
+    sadLittleMan = document.getElementById('littleMan')
+    context = SadLittleMan.getContext('2d')
+    context.beginPath();
+    context.arc(60, 25, 10, 0, Math.PI*2, true);
+    context.stroke();
+}
+
+function draw ($pathFromx, $pathFromy, $pathTox, $pathToy) {
+    
+    context.moveTo($pathFromx, $pathFromy);
+    context.lineTo($pathTox, $pathToy);
+    context.stroke()
+}
+draw()
+        
+function gallow1() {
+    draw (0, 150, 150, 150);
+}
+
+function  gallow2() {
+     draw (10, 0, 10, 600)
+}
+
+ function gallow3() {
+    draw (0, 5, 70, 5);
+        
+}
+
+function gallow4() {
+    draw (60, 5, 60, 15);
+}
+
+function torso() {
+    draw (60, 36, 60, 70);
+}
+
+function rArm() {
+    draw (60, 46, 100, 50);
+}
+
+function lArm() {
+    draw (60, 46, 20, 50);
+}
+
+function rLeg() {
+    draw (60, 70, 100, 100);
+}
+
+function lLeg() {
+    draw (60, 70, 20, 100);
+}
+
+drawArray = [rLeg, lLeg, rArm, lArm,  torso,  head, gallow4, gallow3, gallow2, gallow1];
+
                 
-            }
-            for(i = 0; i < myGuesses.length; i++) {
-                if(counter + words === myGuesses.length) {
-                    showTries.innerHTML = 'You Win!'
-                }
-            }
+play()
+canvas()
+draw()
 
-        }
-        getNumOfTries()
+                
+
+                
+
+
+
+            
+           
+
+            
+            
+            
+
+
+        
+        
+
+
+
+
+
+
+
+
         
     
        
     
         
         
-    }
-    play()
+    
+    
+
     
     
     
-    //  function score () {
-    //     const currentScore = 0
-    //       for (let i = 0; i < currentScore; i++){
-    //           if (words.length === numOfGuesses)
-    //           currentScore += 1
-    //           prompt(`Your score is ${currentScore}`)
-    //           console.log(currentScore)
-    
-              
-    //      }
-    
-    // {
-    
-    //     }
-    
-    //  }
-    //  score()
     
     
     
