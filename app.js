104538e733951e74cd9d39861b5fc2228b9947a2
tamaraandrea <tamara18_1985@msn.com>
2021-11-17 21:35:29 -0500

    play away

--- app.js
+++ app.js
@@ -1,86 +1,81 @@
  // Array of words
-const words = ['jupiter', 'stars', 'astroid', 'black-hole', 'satilite', 'orbit', 'universe', 'umbra', 'lunar', 'space=station', 'astronomy', 'super-nova', 'nebula', 'dark-matter', 'celestial-body']
+const words = ['planet', 'stars', 'astroid', 'moon', 'satilite', 'orbit', 'universe', 'umbra', 'lunar', 'space', 'astronomy', 'eclipse', 'nebula', 'mars', 'meteorite']
 
 // guesses Array
 let myGuesses = []
 
+
 //variables
 let wordSpace = "_"
 let guess = ' '
+let space; //number of spaces in word
 
 
 //score
 let tries = 10
 let counter ;
-
-    
+//Get random word
 let index = Math.floor(Math.random()* words.length)  
-    //play function
+
+//play function
 function play() {
     let userInput = prompt(`would you like to play spaceman? (Y/N)`, "Y")
-        
-    
     console.log(words[index])
+    
         
         
     for(let i = 0; i < words[index].length; i++){
         console.log(words[0][i])
             
-      
-
-            
-    
-            
         let div = document.createElement('div')
+        div.classList.add('letters')
         div.innerHTML=' - '//words[0][i]
-            
         document.querySelector('.word-space').append(div)
-            
-    
-         if(userInput === "Y"){
-            if(words[i] === ' - '){
-                guess.innerHTML = ' - '
-                words.appendChild('.spaces')
-                wordSpace = 1
-                
-            } 
-            guess.innerHTML = ' - '
-    
-                
-                
-    
-                
-        }
+           
     
     }                                        
 }
-
+//handle click function, inactivates buttons once clicked
 let handleclick = e => {
-    e.target.removeEventListener('click',handleclick)
+    e.target.removeEventListener('click', handleclick)
+    e.target.style.backgroundColor= 'grey'
     console.log(e.target.innerHTML)
     myGuesses.push(e.target.innerHTML)
     console.log(myGuesses)
     console.log(words[index].includes(e.target.innerHTML))
     if(words[index].includes(e.target.innerHTML)){
-        
         document.querySelector('.word-space').innerHTML= '  '
+        // let correct = document.createElement('ul')
         
+
         for(let i = 0; i < words[index].length; i++){
+            
+            // correct.setAttribute('id','correctLetters' )
+            // let guess= document.createElement('li')
+            // guess.setAttribute('class','guess')
             console.log(words[0][i])
-            let div = document.createElement('div')
-            if (myGuesses.includes(words[index][i])){
-                div.innerHTML = words[index][i]
+             let div = document.createElement('div')
+             div.classList.add('letter')
+              if (myGuesses.includes(words[index][i])){
+                // if(words[index]=== '- '){
+                    // guess.innerHTML = '-'
+                    space = 1
+                
+                   
+                 div.innerHTML = words[index][i]
                         
                                  
-            } else {
-                div.innerHTML = ' - '
-            }
+              } else {
+                 div.innerHTML = ' - '
+         }
+                
             document.querySelector('.word-space').append(div)
-                    
+            
         }
-           
+             getNumOfTries()         
+            
               
-    } else {
+} else {
         tries -- 
         getNumOfTries()
     
@@ -90,99 +85,75 @@ let handleclick = e => {
 function ans () {
     const buttons = document.querySelectorAll('button')
     buttons.forEach(button => button.addEventListener('click',handleclick))
+    // buttons.forEach(button => button.addEventListener('click', () => 
+    
+    
+    
+
+
+    
 }
 ans()
+
+
 
  
 
 function getNumOfTries (){
+    console.log(tries)
     const showTries = document.querySelector('#myTries')
     showTries.innerHTML = ' You have ' + tries + ' tries'
     if(tries < 1){
-        showTries.innerHTML = 'You loose!'
+       setTimeout(() =>{prompt(`Would you like to try again? (Y,N)`, 'Y')
+       showTries.innerHTML = 'You loose!'
+
+       },2000) 
+      
+       
+        
                
     }
-    for(i = 0; i < myGuesses.length; i++) {
-        if(words[index][i] + tries[i] === myGuesses.length) {
-            showTries.innerHTML = 'You Win!'
-        }
-    }
-}
-getNumOfTries()
-
-//  function animateMan () {
-//     let drawMan = tries ;
-//     drawArray[drawMan]()
-// }
-
+    // if(tries > 0  && words[index].length === myGuesses.length) {
     
-
-// function canvas(){
-//     let littleMan = document.getElementById('man');
-//     // context = littleMan.getContext('2d');
-//     context.beginPath();
-//     context.strokeStyle = 'white';
-//     context.lineWidth = 3;
-// }
-
-// function head(){
-//     sadLittleMan = document.getElementById('littleMan')
-//      context = SadLittleMan.getContext('2d')
-//     context.beginPath();
-//     context.arc(60, 25, 10, 0, Math.PI*2, true);
-//     context.stroke();
-// }
-
-// function draw ($pathFromx, $pathFromy, $pathTox, $pathToy) {
-//     // context.moveTo($pathFromx, $pathFromy);
-//     // context.lineTo($pathTox, $pathToy);
-//     context.stroke()
-// }
-// draw()
+        if(tries > 0  && Array.from(document.querySelectorAll('.letters')).every(letter => letter.innerHTML !== ' - ')) {
+            // showTries.innerHTML = 'You Win!'
+             setTimeout(() =>{alert(`You Win!`)
+             showTries.innerHTML = 'You Win!'
+     
+             },1000)
+            
         
-// function gallow1() {
-//     draw (0, 150, 150, 150);
-// }
-
-// function  gallow2() {
-//      draw (10, 0, 10, 600)
-// }
-
-//  function gallow3() {
-//     draw (0, 5, 70, 5);
+        }
         
-// }
+        
+        
+}
 
-// function gallow4() {
-//     draw (60, 5, 60, 15);
-// }
 
-// function torso() {
-//     draw (60, 36, 60, 70);
-// }
 
-// function rArm() {
-//     draw (60, 46, 100, 50);
-// }
 
-// function lArm() {
-//     draw (60, 46, 20, 50);
-// }
+function reset(){
+    let playAgain= document.getElementsByClassName('playAgain').addEventListener('click', reset)
+    for(let i = 0; i < words[index].length; i++){
+        console.log(words[0][i])
+            
+        let div = document.createElement('div')
+        div.innerHTML=' - '//words[0][i]
+        document.querySelector('.word-space').append(div)
+           
+    
+    } 
+    reset()       
+//   playAgain.addEventListener('click',=)
 
-// function rLeg() {
-//     draw (60, 70, 100, 100);
-// }
+}
+  
 
-// function lLeg() {
-//     draw (60, 70, 20, 100);
-// }
 
-// drawArray = [rLeg, lLeg, rArm, lArm,  torso,  head, gallow4, gallow3, gallow2, gallow1];
 
                 
- play()
-// // canvas()
-// // draw()
+ 
+play()
 
                 
 
